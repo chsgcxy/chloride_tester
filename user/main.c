6087e14ec423e5d58594853eff83e7506805f7e1
chenhao <haohaohao1991@163.com>
2018-10-04 22:14:41 +0800

    Programming specification

--- user/main.c
+++ user/main.c
@@ -12,37 +12,32 @@ static void delay_ms(int ms)
 		for (i = 0; i < 28000; i++);
 }
 
-static void gpio_test(void)
+static int led_init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-	
-	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-
     GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-		while (1) {
-		GPIO_SetBits(GPIOC, GPIO_Pin_10);
-		GPIO_SetBits(GPIOC, GPIO_Pin_11);
-		GPIO_SetBits(GPIOC, GPIO_Pin_12);
-			printf("led on\r\b");
-		delay_ms(1000);
-			GPIO_ResetBits(GPIOC, GPIO_Pin_10);
-		GPIO_ResetBits(GPIOC, GPIO_Pin_11);
-		GPIO_ResetBits(GPIOC, GPIO_Pin_12);
-			delay_ms(1000);
-			printf("led off\r\n");
-		}
+	return 0;
 }
 
 int main(void)
 {
 	uart1_init();
-	gpio_test();
+	gpio_init();
+
+	while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12);
+		printf("led on\r\b");
+		delay_ms(1000);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12);
+		delay_ms(1000);
+		printf("led off\r\n");
+	}
 	return 0;
 }
 
--- user/uart.c
+++ user/uart.c
@@ -3,32 +3,28 @@
 
 int uart1_init(void)
 {	
-	USART_InitTypeDef	 USART_InitStructure;
-  GPIO_InitTypeDef GPIO_InitStruct;
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStruct;
 
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	
-  GPIO_InitStruct.GPIO_Mode=GPIO_Mode_AF;
-  GPIO_InitStruct.GPIO_Speed=GPIO_Speed_50MHz;
-  GPIO_InitStruct.GPIO_OType=GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd=GPIO_PuPd_UP;  
-  GPIO_InitStruct.GPIO_Pin=GPIO_Pin_9 | GPIO_Pin_10;
-  GPIO_Init(GPIOA,&GPIO_InitStruct);
-   	  
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);  
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);	
-  
-  USART_InitStructure.USART_BaudRate            = 115200;
-  USART_InitStructure.USART_WordLength          = USART_WordLength_8b;
-  USART_InitStructure.USART_StopBits            = USART_StopBits_1;
-  USART_InitStructure.USART_Parity              = USART_Parity_No;
-  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStructure.USART_Mode                = USART_Mode_Rx | USART_Mode_Tx;
-  USART_Init(USART1, &USART_InitStructure);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitStruct.GPIO_Mode=GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType=GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd=GPIO_PuPd_UP;  
+	GPIO_InitStruct.GPIO_Pin=GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_Init(GPIOA,&GPIO_InitStruct);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);  
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
 
-  USART_Cmd(USART1, ENABLE);
-  USART_ClearFlag(USART1,USART_FLAG_TXE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 
 	return 0;
 }
@@ -36,11 +32,7 @@ int uart1_init(void)
 int fputc(int ch, FILE *f)
 { 	
 	while((USART1->SR&0X40)==0);  
-	USART1->DR = (u8) ch;      
+	USART1->DR = (u8)ch;      
 	return ch;
 }
 
-
-
-
-	
