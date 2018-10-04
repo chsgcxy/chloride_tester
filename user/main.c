c9310091c3f8564c9badacf1287138901af71595
chenhao <haohaohao1991@163.com>
2018-10-04 20:57:01 +0800

    add a gpio led test, it ok

--- user/main.c
+++ user/main.c
@@ -1,7 +1,42 @@
+#include "stm32f2xx_gpio.h"
+
+static void delay_ms(int ms)
+{
+	int i;
+	
+	while (ms--)
+		for (i = 0; i < 28000; i++);
+}
+
+static void gpio_test(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	
+	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+		while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_10);
+		GPIO_SetBits(GPIOC, GPIO_Pin_11);
+		GPIO_SetBits(GPIOC, GPIO_Pin_12);
+		delay_ms(1000);
+			GPIO_ResetBits(GPIOC, GPIO_Pin_10);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_11);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_12);
+			delay_ms(1000);
+		}
+}
+
 int main(void)
 {
 	
-	
+	gpio_test();
 	return 0;
 }
 
