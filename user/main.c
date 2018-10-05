27610bcfc72ad7510fb0705df07baf7e5a3a779a
chenhao <haohaohao1991@163.com>
2018-10-05 19:35:40 +0800

    porting freertos and test ok

--- user/main.c
+++ user/main.c
@@ -1,6 +1,11 @@
 #include "stm32f2xx_gpio.h"
 #include "stdio.h"
-
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "croutine.h"
+#include "semphr.h"
+#include "event_groups.h"
 
 extern int uart1_init(void);
 
@@ -25,18 +30,63 @@ static int gpio_init(void)
 	return 0;
 }
 
+static void led_task1(void *param)
+{
+	while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_10);
+		vTaskDelay(200);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_10);
+		vTaskDelay(200);
+	}
+}
+
+static void led_task2(void *param)
+{
+	while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_11);
+		vTaskDelay(500);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_11);
+		vTaskDelay(500);
+	}
+}
+
+static void led_task3(void *param)
+{
+	while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_12);
+		vTaskDelay(1000);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_12);
+		vTaskDelay(1000);
+	}
+}
+
+static void task_init(void)
+{
+	xTaskCreate(led_task1, "led1", 64, NULL, 1, NULL);
+    xTaskCreate(led_task2, "led2", 64, NULL, 1, NULL);
+	xTaskCreate(led_task3, "led3", 64, NULL, 1, NULL);
+}
+
 int main(void)
 {
+	/* disable global interrupt, it will be opened by prvStartFirstTask int port.c */
+	__set_PRIMASK(1);
+	/* enable CRC, for stemwin */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_CRC, ENABLE);
+	/* rtos will not run if I set group_2, group_4 is work */
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 	uart1_init();
 	gpio_init();
+	task_init();
 
+	vTaskStartScheduler();
+
+	/* if run here, system error */
 	while (1) {
 		GPIO_SetBits(GPIOC, GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12);
-		printf("led on\r\n");
-		delay_ms(1000);
+		delay_ms(100);
 		GPIO_ResetBits(GPIOC, GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12);
-		delay_ms(1000);
-		printf("led off\r\n");
+		delay_ms(100);
 	}
 	return 0;
 }
